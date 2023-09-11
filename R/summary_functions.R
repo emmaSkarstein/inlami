6751af13d729c67c65870fffa4683dcc7cf4bdbe
Emma Sofie Skarstein <emma@skarstein.no>
2023-09-11 22:36:09 +0200

    add function to create latex matrices from data stacks

--- R/summary_functions.R
+++ R/summary_functions.R
@@ -115,4 +115,82 @@ print.summary.inlami <- function(x, ...){
   cat("\n")
 }
 
+#' Visualize the model data structure as matrices
+#'
+#' @param stack
+#'
+#' @return A latex table?
+#' @export
+#'
+#' @examples
+#' f_moi <- y ~ x + z
+#' f_imp <- x ~ z
+#' stack <- make_inlami_stacks(data = simple_data,
+#'                    formula_moi = f_moi,
+#'                    formula_imp = f_imp,
+#'                    error_type = "classical")
+#' show_data_structure(stack)
+show_data_structure <- function(stack){
+  response_df <- round(stack$data$data, 2)
+  effects_df <- round(stack$effects$data, 2)
 
+  response <- list()
+  effects <- list()
+
+  for(model in names(stack$data$index)){
+    index <- stack$data$index[model][[1]]
+
+    # Building response matrix ----
+    sub_response <- response_df[index, ]
+    sub_response_top <- sub_response[1, ]
+    sub_response_bottom <- sub_response[nrow(sub_response), ]
+
+    vdot_response <- rep("\\vdots", ncol(response_df))
+
+    response <- rbind(response, sub_response_top, vdot_response, sub_response_bottom)
+
+    # Building effects matrix ----
+    sub_effects <- effects_df[index, ]
+    sub_effects_top <- sub_effects[1, ]
+    sub_effects_bottom <- sub_effects[nrow(sub_effects), ]
+
+    vdot_effects <- rep("\\vdots", ncol(effects_df))
+
+    effects <- rbind(effects, sub_effects_top, vdot_effects, sub_effects_bottom)
+  }
+
+  # Constructing response bmatrix
+  response_table <- knitr::kable(response, format = "latex",
+                                 row.names = FALSE, escape = FALSE,
+                                 booktabs = TRUE, linesep = "")
+  response_matrix_body <- gsub('^.*\\\\midrule\\s*|\\s*\\\\bottomrule.*$', '', response_table)
+  response_matrix <- paste0("\\begin{bmatrix} \n", response_matrix_body, "\n\\end{bmatrix}")
+  underbrace_response <- paste0("\\underbrace{", response_matrix, "}_{\\texttt{Y}}")
+
+  # Constructing effects bmatrix vectors
+  effect_matrices <- list()
+
+  for(variable in colnames(effects)){
+    effect_table <- knitr::kable(effects[variable], format = "latex",
+                                 row.names = FALSE, escape = FALSE,
+                                 booktabs = TRUE, linesep = "")
+    effect_matrix_body <- gsub('^.*\\\\midrule\\s*|\\s*\\\\bottomrule.*$', '', effect_table)
+    effect_matrix <- paste0("\\begin{bmatrix} \n", effect_matrix_body, "\n\\end{bmatrix}")
+
+    underbrace_effects <- paste0("\\underbrace{", effect_matrix, "}_{\\texttt{", variable, "}}")
+
+    effect_matrices[[variable]] <- underbrace_effects
+  }
+
+  # Put it all together
+  all_effect_matrices <- paste0(effect_matrices, collapse = " + ")
+  all_of_it <- paste0(underbrace_response, "\n = \n", all_effect_matrices)
+
+  all_of_it_verb <- gsub("NA", "\\\\texttt{NA}", all_of_it)
+
+  message("Make sure to include '\\usepackage{amsmath}', as this is required to display the matrices correctly.")
+
+  return(list(response_df = response,
+              effects_df = effects,
+              matrix_string = all_of_it_verb))
+}
