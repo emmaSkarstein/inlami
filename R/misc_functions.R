5fa78d0af253374a59def1e72799d3358c3835c9
Emma Sofie Skarstein <emma@skarstein.no>
2023-09-27 11:36:36 +0200

    add function to list survival families

---
+++ R/misc_functions.R
@@ -0,0 +1,12 @@
+#' List the survival likelihoods in INLA
+#'
+#' @return List of survival models in INLA
+#'
+#' @examples
+#' inla_survival_families()
+inla_survival_families <- function(){
+  inla_families <- names(INLA::inla.models()$likelihood)
+  surv_families <- inla_families[grepl("surv", inla_families)]
+  surv_with_dots <- gsub("surv", ".surv", surv_families)
+  return(c(surv_families, surv_with_dots, "coxph"))
+}
